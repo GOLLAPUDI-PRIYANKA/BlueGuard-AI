07caf260d6b350d51353a26279fb7f0e339d335e
N. Sri Vaishnavi <n.srivaishnavi26@gmail.com>
2026-09-10 23:15:20 +0530

    Connect API routes to FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from backend.api.routes import router
 
 app = FastAPI(
     title="BlueGuard-AI Backend",
     version="1.0.0"
 )
+
+app.include_router(router)
 
 
 @app.get("/")
