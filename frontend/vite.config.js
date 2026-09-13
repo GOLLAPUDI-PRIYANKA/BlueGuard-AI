362fa2b39b70b300b9addf57b8b5ceec87cbec36
N. Sri Vaishnavi <n.srivaishnavi26@gmail.com>
2026-09-13 22:30:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [".github.dev"],
     fs: {
       allow: [".."],
+    },
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
     },
   },
 });
